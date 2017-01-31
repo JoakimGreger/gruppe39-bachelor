05488c8c0f7c0cde1136fce03fe8c1e772e474a9
JoakimGreger <pedjoa14@student.westerdals.no>
2017-01-31 12:25:46 +0100

    endret fargene på temaet til fargen som the behaviour lab har på siden sin #202026

--- CEapp/app/src/main/java/com/example/joakim/ceapp/MainActivity.java
+++ CEapp/app/src/main/java/com/example/joakim/ceapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.joakim.ceapp;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,73 +14,26 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView resultTxt;
-    ImageView lowImage;
-    ImageView medImage;
-    ImageView fullImage;
-
-    int result;
+    private Button buttonButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lowImage = (ImageView) findViewById(R.id.lowImg);
-        medImage = (ImageView) findViewById(R.id.medImg);
-        fullImage = (ImageView) findViewById(R.id.fullImg);
+        buttonButton = (Button) findViewById(R.id.buttonButton);
 
-        lowImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                lowImage.setColorFilter(Color.GREEN);
-                medImage.setColorFilter(Color.DKGRAY);
-                fullImage.setColorFilter(Color.DKGRAY);
-
-                result = 1;
-
-            }
-        });
-
-        medImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                medImage.setColorFilter(Color.GREEN);
-                lowImage.setColorFilter(Color.DKGRAY);
-                fullImage.setColorFilter(Color.DKGRAY);
-
-               result = 2;
-            }
-        });
-
-        fullImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fullImage.setColorFilter(Color.GREEN);
-                lowImage.setColorFilter(Color.DKGRAY);
-                medImage.setColorFilter(Color.DKGRAY);
-
-               result = 3;
-            }
-        });
-
-        Button buttonButton = (Button) findViewById(R.id.buttonButton);
         buttonButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick (View view) {
-                TextView resultsTxt = (TextView) findViewById(R.id.resultTxt);
-
-                if (result == 1){
-                    resultsTxt.setText("Lav");
-                } if (result == 2){
-                    resultsTxt.setText("Medium");
-                } if (result == 3){
-                    resultsTxt.setText("Høy");
-                }
+                switchActivity();
             }
         });
+
     }
 
-
-
+    private void switchActivity(){
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
 }
