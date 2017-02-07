8052f1773ac6686d01420e7b1843a965672f262a
JoakimGreger <pedjoa14@student.westerdals.no>
2017-02-07 13:33:28 +0100

    Reset score knapp på score screen for testing

--- CEapp/app/src/main/java/com/example/joakim/ceapp/ScoreActivity.java
+++ CEapp/app/src/main/java/com/example/joakim/ceapp/ScoreActivity.java
@@ -4,11 +4,16 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ScoreActivity extends AppCompatActivity {
 
     TextView scoreTxt;
+    ImageView img;
+    Button resetBtn;
     int score;
 
     @Override
@@ -16,10 +21,32 @@ public class ScoreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
         scoreTxt = (TextView) findViewById(R.id.scoreTxt);
+        img =(ImageView) findViewById(R.id.img);
+        resetBtn = (Button) findViewById(R.id.resetBtn);
 
         SharedPreferences prefs = this.getSharedPreferences("qScore", Context.MODE_PRIVATE);
         score = prefs.getInt("qScore", 0);
 
         scoreTxt.setText("" + score);
+
+        img.getLayoutParams().width=(score/2);
+        img.getLayoutParams().height=(score/2);
+
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetScore();
+            }
+        });
+
+    }
+
+    public void resetScore(){
+        //Lagrer tidligere score + nye score som kan hentes på alle activities med riktig key (qScore)
+        SharedPreferences prefs = this.getSharedPreferences("qScore", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("qScore",0);
+        editor.commit();
+        finish();
     }
 }
